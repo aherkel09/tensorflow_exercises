02aaf86fbdada5148979e6439cb8b476d88c537c
Avery Herkel <aherkel09@gmail.com>
2019-07-08 18:11:46 -0400

    get .tsv data from files

--- par_writer.py
+++ par_writer.py
@@ -1,26 +1,52 @@
 import codecs, csv, os
 
-def write_to_file(data):
+def write_to_file(run_data):
     with open('horikawa.par', 'w', newline='') as par:
         writer = csv.writer(par, delimiter='\t')
-        writer.writerow(data.keys()) # headers
-        num_rows = len(data['Duration']) # get list length from arbitrary column
+        writer.writerow(run_data.keys()) # headers
+        num_rows = len(run_data['Duration']) # get list length from arbitrary column
 
         row = 0
         while row < num_rows:
             row_data = []
-            for d in data:
-                row_data.append(data[d][row])
+            for data in run_data:
+                row_data.append(run_data[data][row])
                 
             writer.writerow(row_data)
             row += 1
 
-if __name__ == '__main__':
-    # sample data. real dict should be built from tsv files.
-    data = {
-        'Cumulative_Onset': [1, 2, 3],
-        'Condition_Number_Code': [1, 2, 2],
-        'Duration': [1, 1, 1],
-        'Weight': [1, 1, 1],
+def get_tsv_data():
+    data = {}
+    
+    run = 1
+    while run <= 20:
+        data[run] = get_run_data(run)
+        
+    return data
+
+def get_run_data(run):
+    root_directory = os.getcwd() + '\\tsv_out\\'
+    run_data = {}
+    
+    # map column names to .tsv filenames
+    filename_map = {
+        'Cumulative_Onset': 'onset',
+        'Condition_Number_Code': 'living_nonliving',
+        'Duration': 'duration',
     }
-    write_to_file(data)
+    
+    for fname in filename_map:
+        in_file = root_directory + filename_map[fname] + '-' + run + '.tsv'
+        file_data = []
+        
+        with open(in_file, 'rb') as tsv:
+            reader = csv.reader(codecs.iterdecode(tsv, 'utf-8'), delimter='\t')
+            for row in reader:
+                file_data.append(row)
+        
+        run_data[fname] = file_data
+        
+    return run_data
+
+if __name__ == '__main__':
+    print(get_tsv_data())
