7c503c5c8ec8107f35c9dda0193dfb178d21a110
Avery Herkel <aherkel09@gmail.com>
2019-07-10 10:37:46 -0400

    set file data from known values

--- par_writer.py
+++ par_writer.py
@@ -1,5 +1,7 @@
 import codecs, csv, os
+from pathlib import Path
 
+# FIXME: create file in proper location for all runs & subjects
 def write_to_file(run_data):
     with open('horikawa.par', 'w', newline='') as par:
         writer = csv.writer(par, delimiter='\t')
@@ -21,32 +23,33 @@ def get_tsv_data():
     run = 1
     while run <= 20:
         data[run] = get_run_data(run)
+        run += 1
         
     return data
 
 def get_run_data(run):
-    root_directory = os.getcwd() + '\\tsv_out\\'
-    run_data = {}
-    
-    # map column names to .tsv filenames
-    filename_map = {
-        'Cumulative_Onset': 'onset',
-        'Condition_Number_Code': 'living_nonliving',
-        'Duration': 'duration',
+    root_directory = os.getcwd() + '\\tsv_out'
+    category_file = os.path.join(root_directory, 'living_nonliving-' + str(run) + '.tsv')
+
+    unique_rows = 3 # number of unique rows of data in living_nonliving files
+    run_data = {
+        'Cululative_Onset': [[(36 + 24*n) for n in range(0,25)]]*unique_rows, # onsets per tsv data
+        'Condition_Number_Code': get_codes(category_file),
+        'Duration': [[15 for d in range(0,25)]]*unique_rows, # durations per tsv data
+        'Weight': [[1 for w in range(0, 25)]]*unique_rows, # set all weights to 1
     }
     
-    for fname in filename_map:
-        in_file = root_directory + filename_map[fname] + '-' + run + '.tsv'
-        file_data = []
-        
-        with open(in_file, 'rb') as tsv:
-            reader = csv.reader(codecs.iterdecode(tsv, 'utf-8'), delimter='\t')
-            for row in reader:
-                file_data.append(row)
-        
-        run_data[fname] = file_data
-        
     return run_data
+
+def get_codes(file):
+    # get living_nonliving codes from file
+    codes = []
+    with open(file, 'rb') as tsv:
+        reader = csv.reader(codecs.iterdecode(tsv, 'utf-8'), delimiter='\t')
+        for row in reader:
+            codes.append(row)
+                    
+    return codes
 
 if __name__ == '__main__':
     print(get_tsv_data())
