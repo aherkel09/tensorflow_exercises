40a74468c2f2bc8360cd44c7732936775028d0fa
Avery Herkel <aherkel09@gmail.com>
2019-07-16 15:18:33 -0400

    Update rename_column.py

--- rename_column.py
+++ rename_column.py
@@ -1,7 +1,49 @@
-import os
+import os, csv, shutil
 
 def get_par_files():
+    filepaths = []
     root_dir = os.getcwd()
+    
     for subdirs, dirs, files in os.walk(root_dir):
-        print(subdirs, '\n')
-        print(dirs, '\n')
+        for filename in files:
+            if filename.startswith('horikawa.par'):
+                path = os.path.join(dir, filename)
+                filepaths.append(path)
+                
+    return filepaths
+
+def read_file_data(file):
+    headers = []
+    data = []
+    
+    with open(file, 'r') as par:
+        reader = csv.reader(par, delimiter='\t')
+        headers = next(reader)
+        
+        for row in reader:
+            data.append(row)
+    
+    return headers, data
+    
+def write_file_data(file, headers, data):
+    with open(file, 'w') as par:
+        writer = csv.writer(par, delimiter='\t')
+        writer.writerow(headers)
+        writer.writerows(data)
+
+def rename_column(headers):
+    print('Original:', headers)
+    headers = ['Cumulative_Onset' for h in headers if h == 'Cululative_Onset']
+    print('Corrected:', headers)
+    
+    return headers
+
+if __name__ == '__main__':
+    files = get_par_files()
+    
+    for file in files:
+        headers, data = read_file_data(file)
+        headers = rename_column(headers)
+        write_file_data(file, headers, data)
+        
+    
