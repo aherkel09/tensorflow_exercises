60ec84b43c0997f230f73cd7db3e27b4bdf58b0f
Avery Herkel <aherkel09@gmail.com>
2020-02-28 12:31:15 -0500

    make random pairs, get x of them

--- horikawa/wordnet/data_selector.py
+++ horikawa/wordnet/data_selector.py
@@ -4,24 +4,41 @@ class DataSelector:
     def __init__(self):
         self.conn = sqlite3.connect('offsets/offsets.db')
         self.cursor = self.conn.cursor()
-        self.living, self.nonliving = self.get_data()
+        self.living, self.nonliving = self.get_data('1'), self.get_data('2')
+        self.least_items = min(len(self.living), len(self.nonliving)) - 1 # used to make equal-length lists
     
-    def get_data(self):
-        living = self.cursor.execute('''
-            SELECT wordnet_offset, wordnet_lemma_name 
-            FROM perception_offsets WHERE living_nonliving="1";
-        ''').fetchall()
+    def get_data(self, value):
+        query = str('''
+                    SELECT wordnet_offset, wordnet_lemma_name 
+                    FROM perception_offsets 
+                    WHERE living_nonliving="value";
+                ''').replace('value', value)
         
-        nonliving = self.cursor.execute('''
-            SELECT wordnet_offset, wordnet_lemma_name 
-            FROM perception_offsets WHERE living_nonliving="2";
-        ''').fetchall()
-        
-        return living, nonliving
-    
+        return self.cursor.execute(query).fetchall()
+            
     def random_pair(self):
-        return
+        shuffled_living = random.shuffle(self.living)[:self.least_items]
+        random_living = shuffled_living[random.randint(0, self.least_items)]
+        
+        shuffled_nonliving = random.shuffle(self.nonliving)[:self.least_items]
+        random_nonliving = shuffled_nonliving[random.randint(0, self.least_items)]
+        
+        return (random_living, random_nonliving)
+    
+    def get_x_pairs(x):
+        pairs = []
+        
+        for i in range(x):
+            pairs += [self.random_pair]
+        
+        return pairs
+        
 
 if __name__ == '__main__':
     selector = DataSelector()
-    print(selector.living)
+    
+    # test random_pair()
+    print(selector.random_pair())
+        
+    # test get_x_pairs()
+    print(selector.get_x_pairs(5))
