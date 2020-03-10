7e88ef3f24036b1570622a31872d7658b37cbaf3
Avery Herkel <aherkel09@gmail.com>
2020-03-10 16:15:11 -0400

    Update move_to_endpoint.sh

--- shell_scripts/move_to_endpoint.sh
+++ shell_scripts/move_to_endpoint.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-subject=1
+SUBJECT=1
+TEST=1
+RUN=1
 
-while [ $subject -le 5 ]
+while [ $SUBJECT -le 5 ]
 do
-    scp -r tsv_out/FS_0$subject/bold/* avery@128.205.172.41:horikawa/FS_0$subject/bold
-    (( subject++ ))
+    DIR="sub-0$SUBJECT/ses-perceptionTest0$TEST/func"
+    if [ -d $DIR ]; then
+        echo "$DIR/*.nii.gz bold/$RUN/f.nii.gz"
+        (( RUN++ ))
+    fi
 done
